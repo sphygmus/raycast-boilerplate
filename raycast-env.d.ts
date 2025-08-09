ef78aa8cbee91cd1c2327a555b88a7628d462950
sphygmus <kemalcangedik@mail.com>
2025-08-09 11:29:57 +0300

    initial commit

---
+++ raycast-env.d.ts
@@ -0,0 +1,24 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `example` command */
+  export type Example = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `example` command */
+  export type Example = {}
+}
+
---
+++ src/example.tsx
@@ -0,0 +1,5 @@
+import { Detail } from "@raycast/api";
+
+export default function Command() {
+	return <Detail isLoading={false} navigationTitle="Example Command" markdown="This is an example command." />;
+}
